b4308541e164ecbc3d86b7babff3c5dc97c2ed3f
Andrew W Owens <andrewwowens@Datas-MacBook-Air.local>
2019-07-12 19:00:37 -0400

    basic funciton addition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def cltv_cohort_projection(example_data):
-    from shifted_beta_geometric import derl, fit, predicted_survival
-    alpha, beta = fit(example_data)
-    future = predicted_survival(alpha, beta, len(example_data) + l)[-l:]
-    example_data.extend(future)
-    return example_data
+	from shifted_beta_geometric import derl, fit, predicted_survival
+	alpha, beta = fit(example_data)
+	#future = predicted_survival(alpha, beta, len(example_data) + l)[-l:]
+	#example_data.extend(future)
+	print example_data 
+	return
